1af1df7b6434faca27243e6056c52534993b18e2
KalebMolewe23 <molewekaleb@gmail.com>
2022-05-25 20:15:31 +0700

    update baru autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'calendar', 'encrypt', 'upload', 'form_validation');
+$autoload['libraries'] = array('session', 'calendar', 'upload', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
